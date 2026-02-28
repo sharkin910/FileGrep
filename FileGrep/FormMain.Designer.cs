bbf448002f25eef40507bbb1492823918c7bbd0c
sharkin910 <sharkin910@gmail.com>
2026-02-28 09:18:00 +0900

    initial.

---
+++ FileGrep/FormMain.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace FileGrep
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelPath = new Label();
+            textBoxPath = new TextBox();
+            textBoxLog = new TextBox();
+            buttonProcess = new Button();
+            checkBoxNotInclude = new CheckBox();
+            label1 = new Label();
+            textBoxSearchText = new TextBox();
+            checkBoxIgnoreCase = new CheckBox();
+            checkBoxIgnoreEmptyLine = new CheckBox();
+            checkBoxIgnoreSpaceLine = new CheckBox();
+            labelExtention = new Label();
+            textBoxExtention = new TextBox();
+            checkBoxRecursively = new CheckBox();
+            labelExcludeFolders = new Label();
+            textBoxExcludeFolders = new TextBox();
+            checkBoxAddPathName = new CheckBox();
+            checkBoxAddLineNo = new CheckBox();
+            SuspendLayout();
+            // 
+            // labelPath
+            // 
+            labelPath.AutoSize = true;
+            labelPath.Location = new Point(15, 16);
+            labelPath.Margin = new Padding(4, 0, 4, 0);
+            labelPath.Name = "labelPath";
+            labelPath.Size = new Size(109, 15);
+            labelPath.TabIndex = 0;
+            labelPath.Text = "ファイル・フォルダーパス";
+            // 
+            // textBoxPath
+            // 
+            textBoxPath.AllowDrop = true;
+            textBoxPath.Location = new Point(131, 12);
+            textBoxPath.Margin = new Padding(4);
+            textBoxPath.Name = "textBoxPath";
+            textBoxPath.Size = new Size(780, 23);
+            textBoxPath.TabIndex = 1;
+            textBoxPath.DragDrop += textBoxPath_DragDrop;
+            textBoxPath.DragEnter += textBoxPath_DragEnter;
+            // 
+            // textBoxLog
+            // 
+            textBoxLog.Location = new Point(18, 143);
+            textBoxLog.Margin = new Padding(4);
+            textBoxLog.Multiline = true;
+            textBoxLog.Name = "textBoxLog";
+            textBoxLog.ScrollBars = ScrollBars.Both;
+            textBoxLog.Size = new Size(893, 403);
+            textBoxLog.TabIndex = 2;
+            // 
+            // buttonProcess
+            // 
+            buttonProcess.Location = new Point(18, 104);
+            buttonProcess.Margin = new Padding(4);
+            buttonProcess.Name = "buttonProcess";
+            buttonProcess.Size = new Size(88, 29);
+            buttonProcess.TabIndex = 3;
+            buttonProcess.Text = "実行";
+            buttonProcess.UseVisualStyleBackColor = true;
+            buttonProcess.Click += buttonProcess_Click;
+            // 
+            // checkBoxNotInclude
+            // 
+            checkBoxNotInclude.AutoSize = true;
+            checkBoxNotInclude.Location = new Point(131, 110);
+            checkBoxNotInclude.Margin = new Padding(4);
+            checkBoxNotInclude.Name = "checkBoxNotInclude";
+            checkBoxNotInclude.Size = new Size(228, 19);
+            checkBoxNotInclude.TabIndex = 5;
+            checkBoxNotInclude.Text = "検索文字列を含まない行のほうを出力する";
+            checkBoxNotInclude.UseVisualStyleBackColor = true;
+            checkBoxNotInclude.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 80);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 6;
+            label1.Text = "検索文字列";
+            // 
+            // textBoxSearchText
+            // 
+            textBoxSearchText.Location = new Point(131, 77);
+            textBoxSearchText.Margin = new Padding(4);
+            textBoxSearchText.Name = "textBoxSearchText";
+            textBoxSearchText.Size = new Size(317, 23);
+            textBoxSearchText.TabIndex = 7;
+            // 
+            // checkBoxIgnoreCase
+            // 
+            checkBoxIgnoreCase.AutoSize = true;
+            checkBoxIgnoreCase.Location = new Point(473, 79);
+            checkBoxIgnoreCase.Name = "checkBoxIgnoreCase";
+            checkBoxIgnoreCase.Size = new Size(134, 19);
+            checkBoxIgnoreCase.TabIndex = 8;
+            checkBoxIgnoreCase.Text = "大文字小文字同一視";
+            checkBoxIgnoreCase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIgnoreEmptyLine
+            // 
+            checkBoxIgnoreEmptyLine.AutoSize = true;
+            checkBoxIgnoreEmptyLine.Location = new Point(368, 110);
+            checkBoxIgnoreEmptyLine.Name = "checkBoxIgnoreEmptyLine";
+            checkBoxIgnoreEmptyLine.Size = new Size(121, 19);
+            checkBoxIgnoreEmptyLine.TabIndex = 9;
+            checkBoxIgnoreEmptyLine.Text = "空の行を出力しない";
+            checkBoxIgnoreEmptyLine.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIgnoreSpaceLine
+            // 
+            checkBoxIgnoreSpaceLine.AutoSize = true;
+            checkBoxIgnoreSpaceLine.Location = new Point(498, 110);
+            checkBoxIgnoreSpaceLine.Name = "checkBoxIgnoreSpaceLine";
+            checkBoxIgnoreSpaceLine.Size = new Size(175, 19);
+            checkBoxIgnoreSpaceLine.TabIndex = 10;
+            checkBoxIgnoreSpaceLine.Text = "空白文字だけの行を出力しない";
+            checkBoxIgnoreSpaceLine.UseVisualStyleBackColor = true;
+            // 
+            // labelExtention
+            // 
+            labelExtention.AutoSize = true;
+            labelExtention.Location = new Point(15, 48);
+            labelExtention.Name = "labelExtention";
+            labelExtention.Size = new Size(91, 15);
+            labelExtention.TabIndex = 11;
+            labelExtention.Text = "検索対象拡張子";
+            // 
+            // textBoxExtention
+            // 
+            textBoxExtention.Location = new Point(131, 45);
+            textBoxExtention.Name = "textBoxExtention";
+            textBoxExtention.Size = new Size(110, 23);
+            textBoxExtention.TabIndex = 12;
+            textBoxExtention.Text = "cs;tt";
+            // 
+            // checkBoxRecursively
+            // 
+            checkBoxRecursively.AutoSize = true;
+            checkBoxRecursively.Checked = true;
+            checkBoxRecursively.CheckState = CheckState.Checked;
+            checkBoxRecursively.Location = new Point(271, 47);
+            checkBoxRecursively.Name = "checkBoxRecursively";
+            checkBoxRecursively.Size = new Size(126, 19);
+            checkBoxRecursively.TabIndex = 13;
+            checkBoxRecursively.Text = "下位フォルダーも検索";
+            checkBoxRecursively.UseVisualStyleBackColor = true;
+            // 
+            // labelExcludeFolders
+            // 
+            labelExcludeFolders.AutoSize = true;
+            labelExcludeFolders.Location = new Point(416, 48);
+            labelExcludeFolders.Name = "labelExcludeFolders";
+            labelExcludeFolders.Size = new Size(74, 15);
+            labelExcludeFolders.TabIndex = 14;
+            labelExcludeFolders.Text = "除外フォルダー";
+            // 
+            // textBoxExcludeFolders
+            // 
+            textBoxExcludeFolders.Location = new Point(496, 45);
+            textBoxExcludeFolders.Name = "textBoxExcludeFolders";
+            textBoxExcludeFolders.Size = new Size(415, 23);
+            textBoxExcludeFolders.TabIndex = 15;
+            textBoxExcludeFolders.Text = "obj;bin;log;bak";
+            // 
+            // checkBoxAddPathName
+            // 
+            checkBoxAddPathName.AutoSize = true;
+            checkBoxAddPathName.Location = new Point(683, 110);
+            checkBoxAddPathName.Name = "checkBoxAddPathName";
+            checkBoxAddPathName.Size = new Size(123, 19);
+            checkBoxAddPathName.TabIndex = 16;
+            checkBoxAddPathName.Text = "結果にパス名を付与";
+            checkBoxAddPathName.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAddLineNo
+            // 
+            checkBoxAddLineNo.AutoSize = true;
+            checkBoxAddLineNo.Location = new Point(816, 110);
+            checkBoxAddLineNo.Name = "checkBoxAddLineNo";
+            checkBoxAddLineNo.Size = new Size(95, 19);
+            checkBoxAddLineNo.TabIndex = 17;
+            checkBoxAddLineNo.Text = "行番号を付与";
+            checkBoxAddLineNo.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(924, 561);
+            Controls.Add(checkBoxAddLineNo);
+            Controls.Add(checkBoxAddPathName);
+            Controls.Add(textBoxExcludeFolders);
+            Controls.Add(labelExcludeFolders);
+            Controls.Add(checkBoxRecursively);
+            Controls.Add(textBoxExtention);
+            Controls.Add(labelExtention);
+            Controls.Add(checkBoxIgnoreSpaceLine);
+            Controls.Add(checkBoxIgnoreEmptyLine);
+            Controls.Add(checkBoxIgnoreCase);
+            Controls.Add(textBoxSearchText);
+            Controls.Add(label1);
+            Controls.Add(checkBoxNotInclude);
+            Controls.Add(buttonProcess);
+            Controls.Add(textBoxLog);
+            Controls.Add(textBoxPath);
+            Controls.Add(labelPath);
+            Margin = new Padding(4);
+            Name = "FormMain";
+            Text = "FileGrep - 指定した文字列を含む・含まない行をファイルから抽出する";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.CheckBox checkBoxNotInclude;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSearchText;
+        private CheckBox checkBoxIgnoreCase;
+        private CheckBox checkBoxIgnoreEmptyLine;
+        private CheckBox checkBoxIgnoreSpaceLine;
+        private Label labelExtention;
+        private TextBox textBoxExtention;
+        private CheckBox checkBoxRecursively;
+        private Label labelExcludeFolders;
+        private TextBox textBoxExcludeFolders;
+        private CheckBox checkBoxAddPathName;
+        private CheckBox checkBoxAddLineNo;
+    }
+}
+
---
+++ FileGrep/FormMain.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileGrep
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxPath_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドラッグ＆ドロップされたファイル
+            if (e.Data?.GetData(DataFormats.FileDrop) is not string[] files) return;
+
+            var file = files[0];
+            if (CheckPath(file))
+            {
+                textBoxPath.Text = file;
+            }
+            else
+            {
+                textBoxPath.Text = "";
+            }
+        }
+
+        private void textBoxPath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data?.GetDataPresent(DataFormats.FileDrop) == true)
+            {
+                //ドラッグされたデータ形式を調べ、ファイルのときはコピーとする
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                //ファイル以外は受け付けない
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private bool CheckPath(string path)
+        {
+            if (File.Exists(path))
+            {
+                labelExtention.Enabled = false;
+                textBoxExtention.Enabled = false;
+                checkBoxRecursively.Enabled = false;
+                labelExcludeFolders.Enabled = false;
+                textBoxExcludeFolders.Enabled = false;
+                buttonProcess.Enabled = true;
+                return true;
+            }
+            labelExtention.Enabled = true;
+            textBoxExtention.Enabled = true;
+            checkBoxRecursively.Enabled = true;
+            labelExcludeFolders.Enabled = true;
+            textBoxExcludeFolders.Enabled = true;
+            if (Directory.Exists(path))
+            {
+                buttonProcess.Enabled = true;
+                return true;
+            }
+            buttonProcess.Enabled = false;
+            return false;
+        }
+
+        private void AppendLog(string message)
+        {
+            if (textBoxLog.InvokeRequired)
+            {
+                textBoxLog.Invoke(new Action(() => AppendLog(message)));
+            }
+            else
+            {
+                if (message.EndsWith(Environment.NewLine))
+                {
+                    textBoxLog.AppendText(message);
+                }
+                else
+                {
+                    textBoxLog.AppendText(message + Environment.NewLine);
+                }
+                textBoxLog.SelectionStart = textBoxLog.Text.Length;
+                textBoxLog.ScrollToCaret();
+            }
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            var filePath = textBoxPath.Text.Trim();
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                MessageBox.Show(this, "パス名が指定されていません。");
+                return;
+            }
+            if (!File.Exists(filePath) && !Directory.Exists(filePath))
+            {
+                MessageBox.Show(this, "指定されたパス名が存在しません。");
+                return;
+            }
+
+            var extention = textBoxExtention.Text.Trim();
+            var extentions = extention.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(x => x.StartsWith('.') ? x : "." + x)
+                .ToArray();
+
+            var excludeFolders = textBoxExcludeFolders.Text.Split(";", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                .Select(x => x.StartsWith('\\') ? x : "\\" + x)
+                .Select(x => x.EndsWith('\\') ? x : x + "\\")
+                .ToArray();
+
+            textBoxLog.Clear();
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var log = GrepFile(filePath);
+                    AppendLog(log);
+                }
+                else
+                {
+                    GrepFiles(filePath, extentions, excludeFolders);
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendLog($"エラー: {ex}");
+            }
+        }
+
+        private void GrepFiles(string path, string[] extentions, string[] excludeFolders)
+        {
+            var searchOpt = checkBoxRecursively.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var file in Directory.EnumerateFiles(path, "*.*", searchOpt))
+            {
+                if (IsMatchPath(file, extentions, excludeFolders))
+                {
+                    var log = GrepFile(file);
+                    if (log.Length > 0)
+                    {
+                        AppendLog(log);
+                    }
+                }
+            }
+        }
+
+        private bool IsMatchPath(string filePath, string[] extentions, string[] excludeFolders)
+        {
+            // 指定拡張子以外はスキップ
+            bool hit = false;
+            if (extentions.Length == 0)
+            {
+                // 対象となる拡張子が指定されていないときは全部が対象
+                hit = true;
+            }
+            else
+            {
+                foreach (var ext in extentions)
+                {
+                    if (filePath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
+                    {
+                        hit = true;
+                        break;
+                    }
+                }
+            }
+            if (!hit) return false;
+
+            // 指定フォルダーが含まれていたらスキップ
+            foreach (var folder in excludeFolders)
+            {
+                if (filePath.Contains(folder, StringComparison.OrdinalIgnoreCase))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string GrepFile(string filePath)
+        {
+            string lines = "";
+            string searchText = textBoxSearchText.Text;
+            bool include = !checkBoxNotInclude.Checked;
+            bool addPath = checkBoxAddPathName.Checked;
+            bool addLineNo = checkBoxAddLineNo.Checked;
+            StringComparison comparison = checkBoxIgnoreCase.Checked
+                ? StringComparison.OrdinalIgnoreCase
+                : StringComparison.Ordinal;
+
+            int lineNo = 0;
+            foreach (string line in File.ReadLines(filePath))
+            {
+                lineNo++;
+                if (checkBoxIgnoreEmptyLine.Checked && string.IsNullOrEmpty(line)) continue;
+                if (checkBoxIgnoreSpaceLine.Checked && string.IsNullOrWhiteSpace(line)) continue;
+                if (line.Contains(searchText, comparison) == include)
+                {
+                    if (addPath)
+                    {
+                        if (addLineNo)
+                        {
+                            lines += $"\"{filePath}\"({lineNo}):";
+                        }
+                        else
+                        {
+                            lines += $"\"{filePath}\":";
+                        }
+                    }
+                    else
+                    {
+                        if (addLineNo)
+                        {
+                            lines += $"({lineNo}):";
+                        }
+                    }
+                    lines += line + Environment.NewLine;
+                }
+            }
+            return lines;
+        }
+    }
+}
