4093c2ad3c528fdaffd06a8426a8bc71bb3c7a2e
sharkin910 <sharkin910@gmail.com>
2026-06-03 20:27:35 +0900

    build: add batch file for publish build.

---
+++ publish.bat
@@ -0,0 +1,2 @@
+dotnet publish FileGrep\FileGrep.csproj /p:PublishProfile=FileGrep\Properties\PublishProfiles\FolderProfile.pubxml
+rem dotnet publish FileGrep\FileGrep.csproj -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishReadyToRun=false
